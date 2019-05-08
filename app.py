0965ced37d5dde00901de3a68132df5cba9d761e
Lowe54 <jameslowe241@gmail.com>
2019-05-08 22:01:14 +0000

    Documentation added

--- app.py
+++ app.py
@@ -8,16 +8,20 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/search/')
 def search():
+    '''
+    Performs the actual pagination
+        @arg - page = The current page number
+        @arg - sr = The start index for the results page, 20 means that it will start at index 20
+    '''
     if request.args.get('page') != "":
         page = int(request.args.get('page'))
         sr = int(request.args.get('sr'))
         currentpagenum = int(page)
     resultnum = ['Joseph Allen', 'Autumn Welch', 'Valerie Fields', 'Margaret Mccarty', 'Patricia Yu', 'Kelsey Burton', 'Charles Chandler', 'Michelle Washington', 'Wendy Morris', 'Sherri Perez', 'Holly Campbell', 'Karen Cisneros MD', 'Jessica Perry', 'Kimberly Miranda', 'Michael Lawrence']
     pagecount = 0
-
+    #We set the number to 11, since it is 0 based indexing, otherwise it only shows 9 results
     if len(resultnum) < 11:
-        for c in count:
-            print("No need for extra pages here")
+        print("No need for extra pages here")
     else:
         #Determine the number of result pages
         pages = int(len(resultnum) / 11)
@@ -29,38 +33,6 @@ def search():
             
       
     return render_template('search.html', results=resultnum, c_page=currentpagenum, pc=pagecount, pages=pages, sr=sr, nh=10)
-
-   
-# @app.route('/search/')
-# def search_paginated():
-#     page = int(request.args.get('page'))
-#     sr = int(request.args.get('sr'))
-#     print("sr param is {}".format(sr))
-#     nh = 10
-    
-#     currentpagenum = int(page)
-#     pagecount = 0
-   
-#     if len(resultnum) < 10:
-#         for c in count:
-#             resultnum.append(c)
-#     else:
-#         #Determine the number of result pages
-#         pages = int(len(count) / 10)
-#         if pages == 0:
-#             pagecount = pages
-            
-#         else:
-#             pagecount = pages + 1
-            
-#         #add the number of results to the result list
-        
-#         for c in range(sr,sr+11):
-#             print(c)
-        
-
-       
-#     return render_template('search.html', results=resultnum, c_page=currentpagenum, pc=pagecount, pages=pages, sr=sr, nh=10)
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=False)
