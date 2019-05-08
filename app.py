efd53be5ec0e29e79382ddcbee97e98a61026402
Lowe54 <jameslowe241@gmail.com>
2019-05-08 21:56:21 +0000

    Pagination functionality fixed and working

--- app.py
+++ app.py
@@ -1,64 +1,66 @@
 from flask import Flask, redirect,render_template,request, url_for
+import random
 import os
 app = Flask(__name__)
 
 
 
 @app.route('/')
-@app.route('/search')
-def search():
-    resultnum = []
-    pagecount = 0
-    count = range(1,32)
-    if len(count) < 10:
-        for c in count:
-            resultnum.append(c)
-    else:
-        #Determine the number of result pages
-        pages = int(len(count) / 10)
-        if pages == 0:
-            pagecount = pages
-            for c in count:
-                resultnum.append(c)
-        else:
-            pagecount = pages + 1
-            for c in count:
-                resultnum.append(c)
-
-
-       
-    return render_template('search.html', results=resultnum, pc=pagecount, pages=pages, sr=0, nh=10)
 @app.route('/search/')
-def search_paginated():
-    page = int(request.args.get('page'))
-    sr = int(request.args.get('sr'))
-    nh = 10
-    resultnum = []
-    currentpagenum = int(page)
+def search():
+    if request.args.get('page') != "":
+        page = int(request.args.get('page'))
+        sr = int(request.args.get('sr'))
+        currentpagenum = int(page)
+    resultnum = ['Joseph Allen', 'Autumn Welch', 'Valerie Fields', 'Margaret Mccarty', 'Patricia Yu', 'Kelsey Burton', 'Charles Chandler', 'Michelle Washington', 'Wendy Morris', 'Sherri Perez', 'Holly Campbell', 'Karen Cisneros MD', 'Jessica Perry', 'Kimberly Miranda', 'Michael Lawrence']
     pagecount = 0
-    count = range(1,32)
-    if len(count) < 10:
+
+    if len(resultnum) < 11:
         for c in count:
-            resultnum.append(c)
+            print("No need for extra pages here")
     else:
         #Determine the number of result pages
-        pages = int(len(count) / 10)
+        pages = int(len(resultnum) / 11)
         if pages == 0:
             pagecount = pages
             
         else:
             pagecount = pages + 1
             
-        #add the number of results to the result list
+      
+    return render_template('search.html', results=resultnum, c_page=currentpagenum, pc=pagecount, pages=pages, sr=sr, nh=10)
 
-        for c, i in enumerate(count):
-            print("I is {}".format(i))
-            print(i <= sr)
-            if i <= sr:
-                resultnum.append(i)
+   
+# @app.route('/search/')
+# def search_paginated():
+#     page = int(request.args.get('page'))
+#     sr = int(request.args.get('sr'))
+#     print("sr param is {}".format(sr))
+#     nh = 10
+    
+#     currentpagenum = int(page)
+#     pagecount = 0
+   
+#     if len(resultnum) < 10:
+#         for c in count:
+#             resultnum.append(c)
+#     else:
+#         #Determine the number of result pages
+#         pages = int(len(count) / 10)
+#         if pages == 0:
+#             pagecount = pages
+            
+#         else:
+#             pagecount = pages + 1
+            
+#         #add the number of results to the result list
+        
+#         for c in range(sr,sr+11):
+#             print(c)
+        
 
        
-    return render_template('search.html', results=resultnum, c_page=currentpagenum, pc=pagecount, pages=pages, sr=sr, nh=10)
+#     return render_template('search.html', results=resultnum, c_page=currentpagenum, pc=pagecount, pages=pages, sr=sr, nh=10)
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=False)
